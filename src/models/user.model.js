c5a4250822a81854033aa90936d05ac75de55aaf
mdbayzid315373 <bayzidb315373@gmail.com>
2025-09-12 20:33:40 +0600

    update

---
+++ src/models/user.model.js
@@ -0,0 +1,58 @@
+import mongoose, {Schema} from "mongoose";
+
+const userSchema = new Schema(
+    {
+        username : {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true,
+            index: true
+        },
+        email : {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true,
+        },
+        fullname : {
+            type: String,
+            required: true,
+            trim: true,
+            index: true
+            
+        },
+        avater : {
+            type: String,  //cloudnary url
+            required: true,
+        },
+        coverImage : {
+            type: String, //cloudnary url
+        },
+        watchHistory: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: "Video"
+            }
+        ],
+
+        password: {
+            type: String,
+            required:[true, 'password is required']
+        },
+        refreshToken: {
+            type: String
+        }
+
+    },
+    {
+        timestamps: true
+    }
+)
+
+
+
+
+export const User = mongoose.model("user", userSchema)
