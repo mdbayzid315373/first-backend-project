a87ad070f5fc01c9d7d768d17b1b784efa1297e2
mdbayzid315373 <bayzidb315373@gmail.com>
2025-09-12 18:22:59 +0600

    update

--- src/uitls/asyncHandler.js
+++ src/uitls/asyncHandler.js
@@ -1,6 +1,21 @@
-const asyncHandler = () =>{}
+ const asyncHandler = (requestHandler) =>{
+    (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err))
+    }
+ }
 
 
 
 
 export {asyncHandler}
+
+
+// const asyncHandler = (fn) => async (req, res, next) => {
+//     try {
+        
+//     } catch (error) {
+//         res.status(error.code || 500).json
+//         sucess: false
+//         message: err.massage
+//     }
+// }   
