bab47d64ad1ee9297fc45c51fd3de90e3aad3c20
mdbayzid315373 <bayzidb315373@gmail.com>
2025-09-23 15:01:19 +0600

    update

--- src/app.js
+++ src/app.js
@@ -1,16 +1,29 @@
 import express from "express"
 import cors from "cors"
 import cookieParser from "cookie-parser"
+import userRouter from './routes/user.routes.js'
 
 const app = express()
 app.use(cors({
     origin: process.env.CORS_ORIGIN,
-    Credentials: true
+    credentials: true
 }))
 
 app.use(express.json({limit: "16kb"}))
 app.use( express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
+
+
+//routes import
+
+
+
+//http: //localhost:8000/user/api/v1/users/register
+
+//routes declearation
+app.use("/api/v1/users", userRouter)
+
+
 
 export { app }
